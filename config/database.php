1638966948e3390adb67044dbdfe3946c2c8d21d
Hassaan <hassaan.sagheer5@gmail.com>
2021-12-08 15:40:07 +0500

    Update Database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('DATABASE_URL');
 return [
 
     /*
@@ -15,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +68,16 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'],
+            // 'port' => env('DB_PORT', '5432'),
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL['user'],
+            // 'username' => env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL['pass'],
+            // 'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -123,7 +130,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,7 +1,9 @@
 {{$image}}
-        <img src"asset('storage/upload_images/xhajFcf43rAVq8t8mg8zvu6GI4SlbqadDAi1uNA4.png)">
-        {{-- <img src"{{storage/upload_images/o7OqWPlqQUhYl2vWkK5IXBMT2r8HO0RXjqUQVl1Y.png}}"> --}}
-         <br>
-         {{-- {{ URL('storage/upload_images/o7OqWPlqQUhYl2vWkK5IXBMT2r8HO0RXjqUQVl1Y.png') }} --}}
-        
-         <br><br>
+{{-- {{Storage::disk('local')->get({{$image}})}} --}}
+{{-- <img src"asset('storage/upload_images/xhajFcf43rAVq8t8mg8zvu6GI4SlbqadDAi1uNA4.png)"> --}}
+{{-- <img src"{{storage/upload_images/o7OqWPlqQUhYl2vWkK5IXBMT2r8HO0RXjqUQVl1Y.png}}"> --}}
+{{-- <br> --}}
+{{-- {{ URL('storage/upload_images/o7OqWPlqQUhYl2vWkK5IXBMT2r8HO0RXjqUQVl1Y.png') }} --}}
+
+{{-- <br><br> --}}
+{{-- <img data-src= "{{ Storage::disk('local')->get({{$image}}) }}" alt="Card image cap">  --}}
